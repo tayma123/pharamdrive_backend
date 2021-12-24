97ffac31af68927283f9442775df7859d32aace5
bacemkhlifi <bacemkhlifi001@gmail.com>
2021-12-24 12:50:33 -1100

    add delete categories

--- src/main/java/com/pharamdrive/controller/CategoriesController.java
+++ src/main/java/com/pharamdrive/controller/CategoriesController.java
@@ -2,11 +2,15 @@ package com.pharamdrive.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,10 +32,10 @@ public class CategoriesController {
 	public String  addNewCategorie(@RequestPart("file") MultipartFile file,@RequestParam String name) {
 		Random rand = new Random();
 		Categories categorie = new Categories();
-		String fileName = rand+file.getOriginalFilename();
+		String fileName = file.getOriginalFilename();
 		
 		try {
-			file.transferTo(new File ("F:\\storage\\"+fileName));
+			file.transferTo(new File ("C:\\xampp\\htdocs\\storage\\"+fileName));
 		} catch (IllegalStateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,6 +47,18 @@ public class CategoriesController {
 		categorie.setCategory(name);
 		catRepo.save(categorie);
 		return "nouvelle categorie a ete ajoutee";
+		
+	}
+	//get all categories
+	@GetMapping("/categories")
+	public List<Categories> getAllCategories(){
+		return catRepo.findAll()
+;	}
+	//delete an category
+	@PostMapping("/delete/category/{id}")
+	public String deleteCategory(@PathVariable(value="id") String id) {
+		catRepo.deleteById(id);
+		return "category deleted";
 		
 	}
 
