761914fea9e378788f26a1cdd2572e063fd249c6
bacemkhlifi <bacemkhlifi001@gmail.com>
2022-02-11 23:31:29 -1100

    m

--- src/main/java/com/pharamdrive/controller/MedicamentsContoller.java
+++ src/main/java/com/pharamdrive/controller/MedicamentsContoller.java
@@ -1,6 +1,9 @@
 package com.pharamdrive.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 import javax.websocket.server.PathParam;
 
@@ -12,8 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.pharamdrive.models.Categories;
 import com.pharamdrive.models.Medicaments;
 import com.pharamdrive.models.Users;
 import com.pharamdrive.repository.MedicamentsRepository;
@@ -27,12 +34,34 @@ public class MedicamentsContoller {
 	@Autowired
 	public MedicamentsRepository MedicamentsRepo;
 	@PostMapping(value="/add/medicament")
-	public Medicaments register(@RequestBody Medicaments medicament) {
+	public Medicaments addMEdicaments(@RequestParam String medicament,
+		@RequestParam String prix,@RequestParam String remise,@RequestParam String quantite,@RequestParam String pharmacie
+		,@RequestParam String categorie) {
 		
-		return MedicamentsRepo.save(medicament);
+		
+		Random rand = new Random();
+		Medicaments medic = new Medicaments();
+		/*String fileName = file.getOriginalFilename();
+		
+		try {
+			file.transferTo(new File ("C:\\xampp\\htdocs\\storage\\"+fileName));
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		medic.setMedicament(medicament);
+		medic.setPrix(prix);
+		medic.setRemise(remise);
+		medic.setQuantite(quantite);
+		medic.setPharmacie(pharmacie);
+		medic.setCategorie(categorie);
+		return MedicamentsRepo.save(medic);
 	}
 	@GetMapping("/medicaments")
-	public List<Medicaments> getAllEmployees() {
+	public List<Medicaments> getAllMedicaments() {
 		return MedicamentsRepo.findAll();
 	}
 	//Api Delete medicament
