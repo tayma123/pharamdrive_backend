24710be44a5886b9990ea05c976a9e503326671d
Tayma <taymabenhmida1@gmail.com>
2023-01-18 16:06:39 +0100

    2 commit

---
+++ src/test/java/com/pharamdrive/PharamdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmadrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharamdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
