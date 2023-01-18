de4bd6fe7f9d1b6884bbb4cae4a180bd33813261
Tayma <taymabenhmida1@gmail.com>
2023-01-18 16:25:53 +0100

    6 commit

--- src/test/java/com/pharmadrive/PharamdriveApplicationTests.java
+++ src/test/java/com/pharmadrive/PharamdriveApplicationTests.java
@@ -2,8 +2,9 @@ package com.pharmadrive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = com.pharamdrive.PharamdriveApplication.class)
 class PharamdriveApplicationTests {
 
 	@Test
